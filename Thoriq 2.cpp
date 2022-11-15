aa88762c85616ab582fe02602dc53b22f8c507bc
thoriq2222 <118263539+thoriq2222@users.noreply.github.com>
2022-11-15 17:15:15 +0700

    Add files via upload

---
+++ Thoriq 2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int nilaiTugas[5]={75, 80, 90, 100, 95};
+    for(int i = 0; i<5; i++){
+        cout<<"Nilai tugas-"<<i+1<<" adalah "<< nilaiTugas[i]<<endl;
+    }
+
+    return 0;
+}
